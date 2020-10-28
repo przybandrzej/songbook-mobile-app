2a3fc16c2c6ea35b6b10e98fef22869edb5b3abb
Andrzej <andrzej.przybysz01@gmail.com>
2020-10-28 18:32:34 +0100

    add env specific variables, automate setting environment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import ApiClient from './src/songbook-client/src/ApiClient';
+import { API_URL } from './env.json';
 
-ApiClient.setBasePath('https://stk-uep.pl');
+ApiClient.setBasePath(API_URL);
 AppRegistry.registerComponent(appName, () => App);
